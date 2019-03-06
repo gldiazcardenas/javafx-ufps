28d3044cd76e41b615049a06b8a9c7ae964ebf87
Gabriel Diaz <gabriel.leonardo.diaz@gmail.com>
2019-03-06 17:53:56 -0500

    adjustments

--- JavaFXProject/src/main/java/co/gldc/javafx/ufpsdemo/view/LoginView.java
+++ JavaFXProject/src/main/java/co/gldc/javafx/ufpsdemo/view/LoginView.java
@@ -1,22 +1,16 @@
 package co.gldc.javafx.ufpsdemo.view;
 
-import co.gldc.javafx.ufpsdemo.LoginApp;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class LoginView extends VBox {
 
-    public LoginView() {
-        try {
-            FXMLLoader loader = new FXMLLoader(LoginApp.class.getResource("co/gldc/javafx/ufpsdemo/view/LoginView.fxml"));
-            loader.setController(this);
-            loader.setRoot(this);
-            loader.load();
-            getStylesheets().add(LoginApp.class.getResource("co/gldc/javafx/ufpsdemo/view/LoginView.css").toExternalForm());
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+    @FXML
+    private TextField myTextField;
+
+    @FXML
+    private Label myLabel;
 
 }
